e7a97771ec27bcc2ac885b8aa0f983f01f463f88
boki <lsb530@naver.com>
2021-05-15 15:28:48 +0900

    initial commit

---
+++ src/main/java/com/example/infreanjavatest/InfreanJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.infreanjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfreanJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfreanJavaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/infreanjavatest/StudyTest.java
@@ -0,0 +1,49 @@
+package com.example.infreanjavatest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) // _를 빈 공백문자로 치환
+class StudyTest {
+
+    @Test
+    void create_new_study() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    @Disabled
+    void create_new_study_again() {
+        System.out.println("create1");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after each");
+    }
+}
